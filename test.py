352852b1f8b5138b933eee403a85aad8a2dea1a7
Patson Chidari <patsonchids@gmail.com>
2023-03-09 17:06:41 +0200

    lists

--- test.py
+++ test.py
@@ -60,3 +60,16 @@ pi_string = "3.1415926"
 
 # Convert pi_string into float: pi_float
 pi_float = float(pi_string)
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)

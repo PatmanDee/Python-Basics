17e838d46616efc714d384806ab1db6e46db296b
PatmanDee <patsonchids@gmail.com>
2023-03-10 17:44:36 +0200

    familiar functions

--- test.py
+++ test.py
@@ -144,3 +144,18 @@ areas_copy[0] = 5.0
 
 # Print areas
 print(areas)
+
+#Familiar Functions
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
+out2

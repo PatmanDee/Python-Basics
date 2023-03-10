503f8cdd38ba1295a36bfdac51e8ae83b81c5f59
PatmanDee <patsonchids@gmail.com>
2023-03-10 16:35:19 +0200

    replace list elements

--- test.py
+++ test.py
@@ -107,3 +107,15 @@ print(house)
 
 # Print out the type of house
 print(type(house))
+
+#Replace List Elements
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+areas[-1] = 10.50
+
+# Change "living room" to "chill zone"
+areas[4] = "chill zone"
+
+areas
